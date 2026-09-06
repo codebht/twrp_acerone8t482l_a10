fc41cb4e493d8bb0950f38fd956e294248567eb1
Sebastiano Barezzi <seba@sebaubuntu.dev>
2026-09-06 13:29:27 +0200

    T482L: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: acer Model: Acer One 8 T4-82L Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_T482L.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_T482L-user \
+    omni_T482L-userdebug \
+    omni_T482L-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Acer Acer One 8 T4-82L (T482L)
+
+```
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
